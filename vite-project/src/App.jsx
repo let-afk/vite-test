bc710e3f94b928a85caf2c10d0993a0829c0f02c
Dimaxiton <Dimaxiton5@yandex.ru>
2022-02-11 00:56:30 +0300

    start redux

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.sass';
 import {Router} from './components/Router/Router';
+import {Provider} from 'react-redux';
+import {store} from './store'
 
 function App() {
 
   return (
-    <div className="container">
-      <div className="App">
-        <Router />
+    <Provider store={store}>
+      <div className="container">
+        <div className="App">
+          <Router />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
